6495d53c762f738fe109abda23e3dfd12fbddfca
Israel-covello <israel123.covello@gmail.com>
2021-07-26 17:58:12 -0300

    Inicio do ex-078

---
+++ pythonProject/Exercicios_mundo-3/ex-078.py
@@ -0,0 +1,6 @@
+'''Exercício Python 079: 
+Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente.'''
+
+lista = list()
+while True:
+    lista.append(int(input('Digite um valor: ')))
