87f9bd2b3669dd04c26b5a0f40d63c2078fecd31
Israel-covello <israel123.covello@gmail.com>
2021-07-28 14:39:02 -0300

    ex-078 modificado

--- pythonProject/Exercicios_3/ex-078.py
+++ pythonProject/Exercicios_3/ex-078.py
@@ -1,6 +1,26 @@
-'''Exercício Python 079: 
-Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente.'''
+# ex-078 - Maior e Menorvalor da lista.
 
-lista = list()
-while True:
-    lista.append(int(input('Digite um valor: ')))
+list = []
+maior = 0
+menor = 0
+for l in range(5):
+    list.append(int(input(f'Digite um valor da pocição {l}: ')))
+    if l == 0:
+        maior = menor = list[l]
+    else:
+        if list[l] > maior:
+            maior = list[l]
+        if list[l] < menor:
+            menor = list[l]
+print(f'O Maior valor da lista é {maior} nas posições ', end='')
+for i, v in enumerate(list):
+    if v == maior:
+        print(F'{i}... ', end='')
+print()
+print(f'O menor valor da lista é {menor} nas posições ', end='')
+for i, v in enumerate(list):
+    if v == menor:
+        print(f'{i}... ', end='')
+print()
+
+
