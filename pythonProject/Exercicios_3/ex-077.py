7e4ca1d11e3289b2ae013ec770c764052fe196db
Israel-covello <israel123.covello@gmail.com>
2021-07-28 18:34:44 -0300

    ex-078

---
+++ pythonProject/Exercicios_3/ex-077.py
@@ -0,0 +1,10 @@
+# ex-077 - Contando vogais em Tuplas
+
+palavras = ('Aprender', 'Programa', 'Linguagem', 'Python',
+            'Curso', 'Gratis', 'Estudar', 'Praticar',
+            'Trabalhar', 'Mercado', 'Programador', 'Futuro')
+for p in palavras:
+    print(f'\nA palavra {p.upper()} temos: ', end='')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(f'{letra.upper()}', end=' ')
