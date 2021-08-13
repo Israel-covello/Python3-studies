f39e49d74fff91395bc315403d2ac676caae7d29
Israel Covello <israel123.covello@gmail.com>
2021-08-13 15:46:38 -0300

    complemento ex-098

--- pythonProject/Exercicios_3/ex-098.py
+++ pythonProject/Exercicios_3/ex-098.py
@@ -1,11 +1,23 @@
 # ex-098 - Função de contador
 
 def contador(i, f, p):
-    cont = i
-    while cont <= f:
-        print(f'{cont} ', end='')
-        cont += p
-    print('Fim.')
+    if i < f:
+        cont = i
+        while cont <= f:
+            print(f'{cont} ', end='')
+            cont += p
+        print('\n')
+    else:
+        cont = i
+        while cont >= f:
+            print(f'{cont} ', end='')
+            cont -= p
+        print('\n')
 
 
 contador(1, 10, 1)
+contador(10, 0, 2)
+inicio = int(input('Inicio: '))
+fim = int(input('Final: '))
+passo = int(input('Passo: '))
+contador(inicio, fim, passo)
